c2535efc72d72566008b11e752cccce669990c6a
Zenonist <phonarnun@hotmail.com>
2021-10-10 15:01:33 +0700

    Update Jojo's TileTest file

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -6,17 +6,37 @@ import project_16x16.Tileset;
 import project_16x16.components.Tile;
 
 public class TileTest {
-
+    PImage test_image;
+    Tile newTile;
     @Test
     public void TestGetTileType() {
-        PImage test_image = new Tileset.getTile(298, 291, 9, 9, 4);
-        Tile newTile = new Tile(4,"test_name", test_image, Tile.TileType.COLLISION);
+//        public enum TileType {
+//            COLLISION, BACKGROUND, OBJECT, ENTITY;
+//        }
+        // Tile type COLLISION exists
+        test_image = new Tileset.getTile(298, 291, 9, 9, 4);
+        newTile = new Tile(4,"test_name", test_image, Tile.TileType.COLLISION);
         Assertions.assertEquals(Tile.TileType.COLLISION, newTile.getTileType());
+
+        // Tile type BACKGROUND exists
+        newTile = new Tile(4,"test_name", test_image, Tile.TileType.BACKGROUND);
+        Assertions.assertEquals(Tile.TileType.BACKGROUND, newTile.getTileType());
+
+        // Tile type OBJECT exists
+        newTile = new Tile(4,"test_name", test_image, Tile.TileType.OBJECT);
+        Assertions.assertEquals(Tile.TileType.OBJECT, newTile.getTileType());
+
+        // Tile type ENTITY exists
+        newTile = new Tile(4,"test_name", test_image, Tile.TileType.ENTITY);
+        Assertions.assertEquals(Tile.TileType.ENTITY, newTile.getTileType());
+
+
+
     }
 
     @Test
     public void TestGetPImage() {
-        PImage test_image = new Tileset.getTile(298, 291, 9, 9, 4);
+        PImage test_image = new Tileset.getTile(291, 291, 9, 9, 4);
         Tile newTile = new Tile(4,"test_name", test_image, Tile.TileType.COLLISION);
         Assertions.assertEquals(test_image, newTile.getPImage());
     }
